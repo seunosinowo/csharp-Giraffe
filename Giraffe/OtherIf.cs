70ce0691e05dfd4305e1256ba00c4b52406f85b4
seunosinowo <oluwaseunpaul98@gmail.com>
2026-01-17 13:39:48 +0100

    add OtherIf and While_Loop samples

---
+++ Giraffe/OtherIf.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class OtherIf
+    {
+        public static void OthersIf()
+        {
+            Console.WriteLine(GetMax(3, 66, 7));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //Better Calculator
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            } else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            } else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(GetDay(3));
+
+            Console.ReadLine();
+
+        }
+
+        //Other methods
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            }else if(num2 >= num1 && num2 >= num3){
+                result = num2;
+            }else
+            {
+                result = num3;
+            }
+
+            return result;
+        }
+
+        //Switch Methods
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+            return dayName;
+        }
+    }
+}
---
+++ Giraffe/While_Loop.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class While_Loop
+    {
+        public static void WhileLoop()
+        {
+            int index = 61;
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                //index++;
+            }
+            Console.WriteLine();
+
+            //Guessing game
+            string secretWord = "seun";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }else
+                {
+                    outOfGuesses = true;
+                }   
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You Lose");
+            }else
+            {
+                Console.Write("You win");
+            }
+                
+            Console.ReadLine();
+        }
+    }
+}
